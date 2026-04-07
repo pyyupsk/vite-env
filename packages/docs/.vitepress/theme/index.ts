8cf1a9291e2e19935eeedfe09f4036e12d6ecca9
Pongsakorn Thipayanate <pyyupsk@proton.me>
2026-04-07 17:05:43 +0700

    feat(docs): inject NextBanner into layout-top slot

--- packages/docs/.vitepress/theme/index.ts
+++ packages/docs/.vitepress/theme/index.ts
@@ -1,6 +1,15 @@
 import DefaultTheme from 'vitepress/theme'
+import { h } from 'vue'
+import NextBanner from './NextBanner.vue'
 import '@fontsource-variable/geist'
 import '@fontsource-variable/geist-mono'
 import './custom.css'
 
-export default DefaultTheme
+export default {
+  extends: DefaultTheme,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      'layout-top': () => h(NextBanner),
+    })
+  },
+}
